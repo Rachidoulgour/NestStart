2e7a4001715245b51e346f83bb9015ecbd0a4bfb
Rachidoulgour <olgor@hotmail.com>
2023-12-06 19:24:26 +0100

    findAll and create repository added

--- src/messages/message.repository.ts
+++ src/messages/message.repository.ts
@@ -8,10 +8,16 @@ export class MessagesRespository {
     }
 
     async findAll() {
-
+        const content = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(content);
+        return messages;
     }
 
     async create(message: string) {
-
+        const content = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(content);
+        const id = Math.floor(Math.random() * 999)
+        messages[id] = { id, content: message };
+        await writeFile('messages.json', JSON.stringify(messages));
     }
 }
