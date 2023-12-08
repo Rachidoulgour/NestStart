ee3b3a98bac0a8c7caeed26a15e168e5008fe79d
Rachidoulgour <olgor@hotmail.com>
2023-12-08 18:49:08 +0100

    messagesService added to message controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,27 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message-dto';
+import { MessagesService } from './message.service';    
 
 @Controller('messages')
 export class MessagesController {
+    messagesService: MessagesService;
+    constructor() {
+        this.messagesService = new MessagesService();
+    }
 
     @Get()
     getMessages() {
-
+        return this.messagesService.findAll();
     }
 
     @Post()
     createMessage(@Body() body: CreateMessageDto) {
+        return this.messagesService.create(body.content)
 
     }
 
     @Get('/:id')
     getMessage(@Param('id') id: string) {
-
+        return this.messagesService.findOne(id)
     }
 }
